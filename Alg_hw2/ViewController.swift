788d084fffd9d65706313071696d8a9348f9aeb7
sora910321 <sora910321@gmail.com>
2020-05-22 15:23:50 +0800

    Initial Commit

---
+++ Alg_hw2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alg_hw2
+//
+//  Created by sora on 2020/5/22.
+//  Copyright © 2020 sora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Alg_hw2Tests/Alg_hw2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Alg_hw2Tests.swift
+//  Alg_hw2Tests
+//
+//  Created by sora on 2020/5/22.
+//  Copyright © 2020 sora. All rights reserved.
+//
+
+import XCTest
+@testable import Alg_hw2
+
+class Alg_hw2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
