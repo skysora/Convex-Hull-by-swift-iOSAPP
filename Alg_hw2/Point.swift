79f2d2d46c595d71a81f2425966593463a3c54a7
sora910321 <sora910321@gmail.com>
2020-05-28 15:34:23 +0800

    結束

--- Alg_hw2/Point.swift
+++ Alg_hw2/Point.swift
@@ -7,3 +7,93 @@
 //
 
 import Foundation
+class Point {
+    var x : Int
+    var y : Int
+    init(x:Int,y:Int) {
+        self.x = x
+        self.y = y
+    }
+    // 向量OA叉積向量OB。大於零表示從OA到OB為逆時針旋轉。
+    func cross(o:Point,a:Point ,b:Point) -> Int{
+//        print("A:(\(a.x - o.x),\(a.y - o.y)),B:(\(b.x - o.x),\(b.y - o.y)) = \((a.x - o.x) * (b.y - o.y) - (a.y - o.y) * (b.x - o.x))")
+        return (a.x - o.x) * (b.y - o.y) - (a.y - o.y) * (b.x - o.x);
+    }
+    //(a.y - o.y) * (b.x - o.x)
+    // 兩點距離的平方
+    func compareLen(a:Point,b:Point)->Int{
+        return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y)
+    }
+    func checking(aryTrue:[Point],origin:Point,len:Int) -> [Point] {
+        let points = aryTrue
+//        for i in 0..<len{
+//            print("sorted:(\(Ary[i].x),\(Ary[i].y))")
+//        }
+
+        var answerAry = [Point(x: 0, y: 0)]
+        var flag = 0
+        answerAry[0] = points[0]
+        answerAry.append(points[1])
+        var pointsLen = points.count - 1
+        var count = 2
+        for index in 2..<(points.count){
+            while(true){
+                if(answerAry.count<2){
+                    answerAry.append(points[index])
+                    count = index + 1
+                    break
+                }
+                let result = cross(o: answerAry[(answerAry.count)-2], a: answerAry[(answerAry.count)-1], b: points[index])
+//                print("origin:(\(answerAry[(answerAry.count)-2].x),\(answerAry[(answerAry.count)-2].y)),now:(\(answerAry[(answerAry.count)-1].x),\(answerAry[(answerAry.count)-1].y)),next:(\(points[index].x),\(points[index].y)),cross:\(result)")
+                
+                if(result>=0){
+                    answerAry.append(points[index])
+                    if(index  != points.count - 1  ){
+//                    print("next:\(points[index+1].x),\(points[index+1].y)")
+                    }
+//                    count = index + 1
+                    break
+                }else{
+                    answerAry.removeLast()
+                    continue
+                }
+                count = count + 1
+            }
+        }
+        for i in (0 ... answerAry.count-1){
+             print("前面的:(\(answerAry[i].x),\(answerAry[i].y))")
+        }
+        
+        
+        let re_points = aryTrue.reversed() as [Point]
+        let lowerPartNum = answerAry.count
+        
+        for index in 0..<(re_points.count){
+            while(true){
+                if(answerAry.count<lowerPartNum){
+                    answerAry.append(re_points[index])
+                    break
+                }
+                let result = cross(o: answerAry[(answerAry.count)-2], a: answerAry[(answerAry.count)-1], b: re_points[index])
+                print("origin:(\(answerAry[(answerAry.count)-2].x),\(answerAry[(answerAry.count)-2].y)),now:(\(answerAry[(answerAry.count)-1].x),\(answerAry[(answerAry.count)-1].y)),next:(\(re_points[index].x),\(re_points[index].y)),cross:\(result)")
+                        
+                if(result>=0){
+                    answerAry.append(re_points[index])
+                    if(index  != points.count - 1  ){
+                    print("next:\(points[index+1].x),\(points[index+1].y)")
+                    }
+                    break
+                }else{
+                    answerAry.removeLast()
+                    continue
+                }
+            }
+        }
+        if(answerAry.count != lowerPartNum){
+            for i in (lowerPartNum ... answerAry.count-1){
+                 print("後面的:(\(answerAry[i].x),\(answerAry[i].y))")
+            }
+        }
+        return answerAry
+    }
+}
--- Alg_hw2/ViewController.swift
+++ Alg_hw2/ViewController.swift
@@ -8,13 +8,146 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITextFieldDelegate{
+    var buttonAry = [UIButton()]
+    var positionAry = [Point]()
+    let path = UIBezierPath()
+    let shapeLayer = CAShapeLayer()
+    var answerLen = 0
+    var originPoint = Point(x:0,y:0)
+    var answer = [Point]()
+    var accPosition = [Int]()
+    var base = 0.0
+    @IBOutlet weak var backGround: UIView!
+    @IBOutlet weak var count: UITextField!
+    @IBOutlet weak var pointSize: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        buttonAry.popLast()
+        count.delegate = self
+        pointSize.delegate = self
+        accPosition.popLast()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+          textField.resignFirstResponder()
+          return true
+    }
+    @IBAction func clearBtn(_ sender: Any) {
+        for view in backGround.subviews {
+            view.removeFromSuperview()
+        }
+        buttonAry = []
+        positionAry = []
+        answer = []
+        answerLen = 0
+        accPosition = []
+        self.backGround.layer.sublayers?.removeAll()
+    }
+    func containsDuplicate(nums: [Point],target:Point) -> Bool {
+        for point in nums{
+            if(point.x == target.x && point.y == target.y){
+                return false
+            }
+        }
+        return true
+    }
+    @IBAction func clickBtn(_ sender: UIButton) {
+        let  countNumber = Int(count.text!)!
+        let pointSizeNumber = Double(pointSize.text!)!
+        let  count = 10
+        //產生random
+        for i in 0..<countNumber{
+            let btn = UIButton()
+            btn.backgroundColor = .red
+            var x = 0
+            var y = 0
+            repeat{
+                x = Int.random(in: 0...count)
+                y = Int.random(in: 0...count)
+            }while(!containsDuplicate(nums:positionAry,target:Point(x: x, y: y)))
+            positionAry.append(Point(x:x,y:y))
+            base = Double(backGround.frame.width) / Double(count)
+            btn.frame = CGRect(x: Double(base)*Double(x), y: Double(backGround.frame.width) - Double(base)*Double(y), width: pointSizeNumber, height: pointSizeNumber)
+            btn.layer.cornerRadius = 5
+            buttonAry.append(btn)
+            backGround.addSubview(btn)
+        }
+        
+        //排序
+        for j in 0..<(positionAry.count-1){
+            for k in (j...(positionAry.count-2)).reversed(){
+                if (positionAry[k].x > positionAry[k+1].x){
+                    var temp : Point
+                    temp = positionAry[k]
+                    positionAry[k] = positionAry[k+1]
+                    positionAry[k+1] = temp
+                    var tempBtn : UIButton
+                    tempBtn = buttonAry[k]
+                    buttonAry[k] = buttonAry[k+1]
+                    buttonAry[k+1] = tempBtn
+                }
+            }
+        }
+        for j in 0..<(positionAry.count-1){
+            for k in (j...(positionAry.count-2)).reversed(){
+                if (positionAry[k].x == positionAry[k+1].x && positionAry[k].y > positionAry[k+1].y){
+                    var temp : Point
+                    temp = positionAry[k]
+                    positionAry[k] = positionAry[k+1]
+                    positionAry[k+1] = temp
+                    var tempBtn : UIButton
+                    tempBtn = buttonAry[k]
+                    buttonAry[k] = buttonAry[k+1]
+                    buttonAry[k+1] = tempBtn
+                }
+            }
+        }
+//        for i in positionAry{
+//            print("\(i.x),\(i.y)")
+//        }
+//        送答案去檢查
+        answer = originPoint.checking(aryTrue:positionAry, origin: positionAry[0], len: countNumber )
+//        answer.append(answer[0])
+        for i in answer{
+            print("\(i.x),\(i.y)")
+        }
+        print("==================")
+        for i in answer{
+            for j in 0...(positionAry.count - 1){
+                if(i.x == positionAry[j].x && i.y == positionAry[j].y){
+                    accPosition.append(j)
+                }
+            }
+        }
+//        for i in accPosition{
+//            print(i)
+//        }
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: buttonAry[accPosition[0]].frame.origin.x, y: buttonAry[accPosition[0]].frame.origin.y))
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+        shapeLayer.lineWidth = 3
+        shapeLayer.strokeColor = UIColor(red: 65/255, green: 65/255, blue: 65/255, alpha: 1).cgColor
+                
+        backGround.layer.addSublayer(shapeLayer)
+        print("============")
+        for i in 0..<accPosition.count{
+            if(i != 0){
+                path.addLine(to: CGPoint(x: buttonAry[accPosition[i]].frame.origin.x, y: buttonAry[accPosition[i]].frame.origin.y))
+                let shapeLayer = CAShapeLayer()
+                shapeLayer.path = path.cgPath
+                shapeLayer.lineWidth = 3
+                shapeLayer.strokeColor = UIColor(red: 65/255, green: 65/255, blue: 65/255, alpha: 1).cgColor
+                shapeLayer.fillColor = UIColor(red: 65/255, green: 65/255, blue: 65/255, alpha: 0).cgColor
+                backGround.layer.addSublayer(shapeLayer)
+            }
+//            print("\(answer[i].x),\(Double(answer[i].y))")
+            buttonAry[accPosition[i]].backgroundColor = .black
+        }
+    }
 }
+
 
